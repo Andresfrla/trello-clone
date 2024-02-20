e7f065999033dbbe34df406ea1d4b367688356db
Andres Franco <andresfranla@gmail.com>
2024-02-20 16:34:35 -0600

    config the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
   publicRoutes: ['/'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  afterAuth(auth, req){
+    if(auth.userId && auth.isPublicRoute){
+      let path = "/select-org"
+
+      if(auth.orgId){
+        path = `/organization/${auth.orgId}`
+      }
+
+      const orgSelection = new URL(path, req.url)
+      return NextResponse.redirect(orgSelection)
+    }
+  }
 });
  
 export const config = {
