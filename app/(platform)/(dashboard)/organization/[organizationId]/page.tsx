9c5a10eba105c6fc2a22abaed6b7efb550ef5adc
Andres Franco <andresfranla@gmail.com>
2024-02-23 15:13:49 -0600

    create the submit form for the boards

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,23 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs"
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button"
 
 const OrganizationIdPage = () => {
+  
+
   return (
     <div>
-        Organization Page
+        <form action={create }>
+          <input 
+            id="title"
+            name="title"
+            required
+            placeholder="Enter a Board Title"
+            className="border-black border p-1"
+          />
+          <Button type="submit">
+            Submit
+          </Button>
+        </form>
     </div>
   )
 }
