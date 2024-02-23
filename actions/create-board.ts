180c26a2337ced2b3e8b26492426cf68acc761d9
Andres Franco <andresfranla@gmail.com>
2024-02-23 15:46:55 -0600

    pending fix the code

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,13 +1,42 @@
 "use server"
 
-import { db } from "@/lib/db"
+import { z } from "zod"
 
-export async function create(formData: FormData){
-    const title = formData.get("title") as string
+import { db } from "@/lib/db"
+import { revalidatePath } from "next/cache";
+
+export type State = {
+  errors?: {
+    title?: string[];
+  },
+  message?: string | null;
+}
+
+const CreateBoard = z.object({
+    title: z.string().min(3, {
+      message: "Minimun length of 3 letters is required"
+    })
+});
+
+export async function create(prevState: State,formData: FormData){
+    const validatedFields = CreateBoard.safeParse({
+        title: formData.get("title")
+    })
+
+    if(!validatedFields){
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: "Missing fields"
+      }
+    }
+
+    const { title } = validatedFields.data
 
     await db.board.create({
       data: {
         title,
       }
     })
+
+    revalidatePath(`/organization/org_2cgk5nzlKtL4vl8AzLF6rzeUmrv`)
   }
