1ea89adc5c8d7f00b33b1117f08f9ec46f730e7d
Andres Franco <andresfranla@gmail.com>
2024-02-27 14:11:12 -0600

    finish validator

--- app/(platform)/(dashboard)/organization/[organizationId]/form-delete.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form-delete.tsx
@@ -7,8 +7,6 @@ import { useFormStatus } from 'react-dom'
 export const FormDelete = () => {
     const { pending } = useFormStatus()
 
-    
-
   return (
     <Button disabled={pending} type="submit" variant="destructive" size="sm">
         Delete
