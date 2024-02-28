e78e35f237116c7aa24f6e2fbd8bcc7b910754fb
Andres Franco <andresfranla@gmail.com>
2024-02-28 10:39:44 -0600

    add the submit form on the boards

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,7 +1,9 @@
-import { User2 } from "lucide-react"
+import { FormPopover } from "@/components/form/form-popover"
+import { Hint } from "@/components/hint"
+import { HelpCircle, User2 } from "lucide-react"
 
 export const BoardList = () => {
-    
+
 
   return (
     <div className="space-y-4">
@@ -10,7 +12,8 @@ export const BoardList = () => {
             Your Boards
         </div>
         <div className="grid gris-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-            <div
+            <FormPopover sideOffset={10} side="right">
+                <div
                 role="button"
                 className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
             >
@@ -20,7 +23,19 @@ export const BoardList = () => {
                 <span className="text-sm">
                     5 remaining
                 </span>
-            </div>
+                <Hint
+                    sideOffset={40}
+                    description={`
+                        Free Workspace can have up to 5 open boards. For unlimited boards upgrade
+                        this workspace.
+                    `}
+                >
+                    <HelpCircle 
+                        className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+                    />
+                </Hint>
+                </div>
+            </FormPopover>
         </div>
     </div>
   )
